1c854a73d1293bf9ad21d4bf509c63ab1b948af4
ThinBones <145872474+ThinBones@users.noreply.github.com>
2024-11-12 19:56:47 -0600

    Finished InsertionAndSelection.java

--- InsertionAndSelection.java
+++ InsertionAndSelection.java
@@ -2,11 +2,13 @@ import java.util.*;
 import java.io.*;
 
 public class InsertionAndSelection {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ArrayList<Integer> list = genRandomList();
         Integer[] arr = genRandomArray();
+        print(list, arr, "Before sort:\n");
         insertionSort(list);
         selectionSort(arr);
+        print(list, arr, "After sort:\n");
     }
 
     public static ArrayList<Integer> genRandomList() {
@@ -14,7 +16,7 @@ public class InsertionAndSelection {
         ArrayList<Integer> alist = new ArrayList<Integer>();
         
         for(int i = 0; i < 100; i++) {
-            alist.add(random.nextInt());
+            alist.add(random.nextInt(100));
         }
 
         return alist;
@@ -25,35 +27,33 @@ public class InsertionAndSelection {
         Integer[] intArr = new Integer[100];
 
         for(int i = 0; i < 100; i++) {
-            intArr[i] = random.nextInt();
+            intArr[i] = random.nextInt(100);
         }
 
         return intArr;
     }
 
     public static void insertionSort(ArrayList<Integer> a) {
-        for(int currentSortedIndex = 1; currentSortedIndex < a.size(); currentSortedIndex++) {
-            int nextElement = a.get(currentSortedIndex);
-            int compareI = currentSortedIndex - 1;
-            while((compareI > 0) && (a.get(compareI) > nextElement)) {
-                a.set(compareI, a.get(compareI - 1));
+        for(int i = 1; i < a.size(); i++) {
+            int nextElement = a.get(i);
+            int compareI = i - 1;
+            while((compareI >= 0) && (a.get(compareI) > nextElement)) {
+                a.set(compareI + 1, a.get(compareI));
                 compareI--;
             }
-            a.set(compareI, nextElement);
+            a.set(compareI + 1, nextElement);
         }
     }
 
-    public static void selectionSort(Comparable<Integer> a[]) {
-        for(int endOfArrayIndex = a.length; endOfArrayIndex > 0; endOfArrayIndex--) {
-            Comparable<Integer> max = a[0];
+    public static void selectionSort(Comparable<Integer>[] a) {
+        for(int i = a.length - 1; i > 0; i--) {
             int maxIndex = 0;
-            for(int i = 0; i < endOfArrayIndex; i++) {
-                if(a[i].compareTo((Integer) max) > 0) {
-                    max = a[i];
-                    maxIndex = i;
+            for(int j = 1; j <= i; j++) {
+                if(a[j].compareTo((Integer) a[maxIndex]) > 0) {
+                    maxIndex = j;
                 }
-                swapArray(a, maxIndex, endOfArrayIndex);
             }
+            swapArray(a, maxIndex, i);
         }
     }
 
@@ -65,7 +65,23 @@ public class InsertionAndSelection {
         }
     }
 
-    public static void print(ArrayList<Integer> a, Comparable<Integer> b[], String msg) throws FileNotFoundException, IOException {
-        FileWriter     file   = new FileWriter("output.txt");
+    public static void print(ArrayList<Integer> a, Comparable<Integer>[] b, String msg) throws IOException {
+        FileWriter fw = new FileWriter("output", true);
+        fw.write(msg);
+    
+        for (int i = 0; i < a.size(); i++) {
+            fw.write(a.get(i).toString() + " ");
+            if ((i + 1) % 10 == 0) fw.write("\n");
+        }
+    
+        fw.write("\n");
+    
+        for (int i = 0; i < b.length; i++) {
+            fw.write(b[i].toString() + " ");
+            if ((i + 1) % 10 == 0) fw.write("\n");
+        }
+    
+        fw.write("\n\n");
+        fw.close();
     }
 }
